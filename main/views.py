b8fb147c9e9a0460d4960a4d2990cad1d7865483
QAlex68 <qalex68@gmail.com>
2025-08-22 13:40:31 +0300

    Добавили app main, страницы index, about

--- main/views.py
+++ main/views.py
@@ -1,19 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import context
+
 
 
 def index(request) -> HttpResponse:
     context: dict = {
-        'tile': 'Home',
-        'content': 'Главная страница магазина - HOME',
-        'list': ['First', 'second'],
-        'dict': {'first': 1},
-        'bool': True,
-        'is_authenticated': False,
+        'tile': 'Home - Главная',
+        'content': 'Магазин мебели - HOME'
     }
-
     return render(request, 'main/index.html', context)
 
 def about(request) -> HttpResponse:
-    return HttpResponse('About page')
+    context: dict = {
+        'tile': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том почему мы такие клевые и самые лучшие.',
+    }
+
+    return render(request, 'main/about.html', context)
