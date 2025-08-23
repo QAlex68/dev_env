9410f3cdc924a1071bd9c495dcdb1bb350599291
QAlex68 <qalex68@gmail.com>
2025-08-23 20:07:51 +0300

    Добавлены фикстуры для goods

--- main/views.py
+++ main/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 
 def index(request) -> HttpResponse:
+
+    categories = Categories.objects.all()
+
     context: dict = {
         'tile': 'Home - Главная',
-        'content': 'Магазин мебели - HOME'
+        'content': 'Магазин мебели - HOME',
+        'categories': categories
     }
     return render(request, 'main/index.html', context)
 
